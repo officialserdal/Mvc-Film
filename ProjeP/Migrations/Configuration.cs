4b91fdba13f0a1e28d0cb92575b004edfbb01f60
Serdal58 <sserdalyildirim58@gmail.com>
2023-08-19 23:06:49 +0300

    ..
    ..

---
+++ ProjeP/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjeP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProjeP.Models.context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ProjeP.Models.context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- ProjeP/Models/Sınıflar/Film.cs
+++ ProjeP/Models/Sınıflar/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,12 @@ namespace ProjeP.Models.Sınıflar
 {
     public class Film
     {
-
+        [Key]
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Tur { get; set; }
+        public string Aciklama { get; set; }
+        public int Yil { get; set; }
 
     }
 }
