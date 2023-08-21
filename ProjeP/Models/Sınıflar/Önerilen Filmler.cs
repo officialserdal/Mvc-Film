1af72ac78fa85ed081e1ce0292135d76b84110ef
Serdal58 <sserdalyildirim58@gmail.com>
2023-08-21 16:07:53 +0300

    .
    ..

---
+++ ProjeP/Models/Sınıflar/Önerilen Filmler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjeP.Models.Sınıflar
+{
+    public class Önerilen_Filmler
+    {
+
+        public int İD { get; set; }
+        public string FilmAdi { get; set; }
+        public string YayinYili { get; set; }
+        public string Açiklama { get; set; }
+        public string FotoUrl { get; set; }
+    }
+}
